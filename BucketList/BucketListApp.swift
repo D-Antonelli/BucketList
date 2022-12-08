115df7a7fcf8e4523ad7836e8a0f235b647e31e2
derya <derya@boonandmoil.dev>
2022-12-08 08:42:16 +0000

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Derya Antonelli on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
