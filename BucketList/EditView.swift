4c8c00158d03fec1f0d173b6cb4d2df046e5a850
derya <derya@boonandmoil.dev>
2022-12-15 14:58:40 +0300

    moves inline sort function to data

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -43,7 +43,7 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
-                            Text("Page description here")
+                            Text(page.description)
                                 .italic()
                         }
                     case .loading:
@@ -74,7 +74,7 @@ struct EditView: View {
     
     func fetchNearbyPlaces() async {
         let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-
+        
         guard let url = URL(string: urlString) else {
             print("Bad URL: \(urlString)")
             return
@@ -86,11 +86,11 @@ struct EditView: View {
             let items = try JSONDecoder().decode(FetchResult.self, from: data)
             
             // success – convert the array values to our pages array
-            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
             // if we're still here it means the request failed somehow
-                    loadingState = .failed
+            loadingState = .failed
         }
     }
 }
